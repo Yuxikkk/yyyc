fd475bf8c53cd9a3c431b05f3e386b486fcc0c7a
Yuxikkk <1925844829@qq.com>
2023-11-29 18:47:27 +0800

    自动推送

--- mytools.py
+++ mytools.py
@@ -110,3 +110,22 @@ def plot_variable_pie(data_table, variable):
     
 
     plt.show()
+
+import pandas as pd
+from scipy import stats
+def 单变量参数估计(file_path, confidence_level): 
+  file_path = R"movie_data_cleaned.csv"
+  df_movies = pd.read_csv(file_path)
+# 计算均值和标准误差
+  mean = df_movies['average'].mean()
+  std_error = stats.sem(df_movies['average'])
+# 设定置信水平
+  confidence_level = 0.95
+# 设定自由度
+  自由度 =  len(df_movies['average']) - 1
+# 计算置信区间
+  confidence_interval = stats.t.interval(confidence_level, 自由度, loc=mean, scale=std_error)
+# 输出结果
+  print(F"均值：{mean: .2f}")
+  print(F"均值在置信水平{confidence_level}下的置信区间为：", confidence_interval)
+
